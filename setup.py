4ba49d558f0eec57b05ad9d2a895c718b1ba8f78
Mateusz Woś <wos.mateusz16@gmail.com>
2017-05-08 18:28:36 +0200

    last fix. done

--- setup.py
+++ setup.py
@@ -2,8 +2,9 @@ from setuptools import setup
 
 setup(name='complexity_timer',
       version='1.0',
-      description='Program for measuring a complexity time of functions',
-      author='mwoss',
+      description='Program for measuring a complexity of functions',
+      url='https://github.com/AGHPythonCourse2017/zad2-mwoss',
+      author='Mateusz Wos',
       author_email='wos.mateusz16@gmail.com',
       license='MIT',
       packages=['complexity'],
